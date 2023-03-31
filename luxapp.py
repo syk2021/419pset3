7cff7da2caff6637b6d83f02aaaa6675c1951ca9
syk2021 <sophia.kang@yale.edu>
2023-03-31 14:43:33 -0400

    luxapp changes

--- luxapp.py
+++ luxapp.py
@@ -21,6 +21,10 @@ def index():
         text-align: right;
         padding-right: 10px;
       }
+      .container{
+      text-align: center;
+      width: 350px;
+      }
     </style>"""
     html += get_header()
     html += '<form action="search" method="get">'
@@ -28,8 +32,19 @@ def index():
     html += '<div><label>Classification</label><input type="text" name="Classification"></div>'
     html += '<div><label>Agent</label><input type="text" name="Agent"></div>'
     html += '<div><label>Department</label><input type="text" name="Department"></div>'
+    html += '<div class="container">'
+    html += '<input id="btn" type="submit" value="Search">'
+    html += '</div>'
     html += '</form>'
     html += '</html>'
 
     response = make_response(html)
     return response
+
+@app.route('/search', methods=['GET'])
+def search():
+    Label = request.args.get('Label')
+    Classification = request.args.get('Classification')
+    Agent = request.args.get('Agent')
+    Department = request.args.get('Department')
+    
