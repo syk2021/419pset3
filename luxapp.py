fb1ef5eba1940f654b4df4dd9ec35c99285d1af5
syk2021 <sophia.kang@yale.edu>
2023-04-06 19:16:55 -0400

    setting cookies and html

--- luxapp.py
+++ luxapp.py
@@ -10,12 +10,6 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-
-    html = get_style()
-    html += get_header()
-    html += get_form()
-    html += get_footer()
-
     html = render_template('index.html', time=asctime(localtime()))
     response = make_response(html)
     return response
@@ -23,20 +17,24 @@ def index():
 
 @app.route('/search', methods=['GET'])
 def search():
+    # get cookie values
+    prev_label = request.cookies.get('prev_label')
+    if not prev_label:
+        prev_label = ""
+    prev_classifier = request.cookies.get('prev_classifier')
+    if not prev_classifier:
+        prev_classifier = ""
+    prev_agent = request.cookies.get('prev_agent')
+    if not prev_agent:
+        prev_agent = ""
+    prev_department = request.cookies.get('prev_department')
+    if not prev_department:
+        prev_department = ""
+
     label_res = request.args.get('Label')
     classification_res = request.args.get('Classification')
     agent_res = request.args.get('Agent')
     department_res = request.args.get('Department')
-
-    html = get_style()
-    html += get_header()
-
-    if not label_res and not classification_res and not agent_res and not department_res:
-        html += "No search terms provided. Please enter some search terms."
-        html += get_form()
-        html += get_footer()
-        response = make_response(html)
-        return response
 
     search_response = LuxQuery(DB_NAME).search(agt=agent_res, dep=department_res,
                                                classifier=classification_res, label=label_res)
@@ -44,44 +42,16 @@ def search():
     print(search_response)
     print(type(search_response))
     response_data = search_response["data"]
-
-    row_gen = ""
+    data = []
 
     for row in response_data:
-        row_gen += '<tr>'
-        # Label
-        row_gen += f'<td>{row[1]}</td>'
-        # Date
-        row_gen += f'<td>{row[2]}</td>'
-        # Agents
-        row_gen += f'<td>{row[3]}</td>'
-        # Classified As
-        row_gen += f'<td>{row[4]}</td>'
-        row_gen += '</tr>'
+        data.append(row[1:])
 
-    html += get_form()
-
-    html += f"""
-    <table>
-        <thead>
-            <tr>
-                <th>Label</th>
-                <th>Date</th>
-                <th>Agents</th>
-                <th>Classified As</th>
-            </tr>
-        </thead>
-        <tbody>
-        {row_gen}
-        </tbody>
-    </table>
-    """
-
-    html += get_footer()
-
+    html = render_template('index.html', time=asctime(localtime()), table_data=data, prev_label=prev_label, prev_classifier=prev_classifier, prev_agent=prev_agent, prev_department=prev_department)
     response = make_response(html)
     response.set_cookie('prev_label', label_res)
     response.set_cookie('prev_classifier', classification_res)
     response.set_cookie('prev_agent', agent_res)
     response.set_cookie('prev_department', department_res)
+
     return response
